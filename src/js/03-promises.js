fa5ccb9eb4b7028bf31563b5b3b06ccc729a24b7
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-04-19 00:45:50 +0400

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { reject } from 'lodash';
+import { resolve } from 'path-browserify';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
+}
+const delayForm = document.querySelector('.form');
+let interval = null;
+let amountValue = 0;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  console.log(Number(step.value));
+  setTimeout(() => {
+    interval = setInterval(() => {
+      amountValue += 1;
+      if (amountValue <= Number(amount.value)) {
+        createPromise(amountValue, Number(delay.value))
+          .then(onSuccess)
+          .catch(onError);
+      } else {
+        clearInterval(interval);
+      }
+    }, Number(step.value));
+  }, Number(delay.value));
+}
+
+delayForm.addEventListener('submit', handleSubmit);
+
+function onSuccess(result) {
+  Notify.success(result);
+}
+
+function onError(result) {
+  Notify.failure(result);
 }
